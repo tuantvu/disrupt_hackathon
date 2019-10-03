ab0c04ea9a80faaef888a49d6faa0e7f4a1c6579
tuanvu <19482735+tuantvu@users.noreply.github.com>
2019-10-03 11:31:10 -0700

    animal csv

--- dataobjects/animal.ts
+++ dataobjects/animal.ts
@@ -1,25 +1,25 @@
 import { Photo } from "./photo";
 
 export interface Animal {
-    id: string,
-    organization_id: string,
+    id: string, //db
+    organization_id: string, //db
     url: string,
-    type: string,
-    species: string,
+    type: string, //db
+    species: string, //db
     breeds: Breed,
     colors: Color,
-    age: string,
+    age: string, //db
     gender: string,
     size: string,
     coat: string,
-    name: string,
+    name: string, //db
     description: string,
     photos: Photo[],
-    status: string,
-    attributes: Attributes,
+    status: string, //db
+    attributes: Attributes, //db
     tags: string[],
-    published_at: string,
-    status_changed_at: string,
+    published_at: string, //db
+    status_changed_at: string, //db
 }
 
 export interface Breed {
@@ -42,3 +42,4 @@ export interface Attributes {
     special_needs: boolean,    
     shots_current: boolean,
 }
+
